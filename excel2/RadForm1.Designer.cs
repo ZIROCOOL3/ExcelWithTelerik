ff4323da55b99ccf346d779471616b13af0f5a91
zirocool2020 <zirocool2003@hotmail.com>
2022-09-02 11:37:21 -0300

    02-09-22
    abir y leer excel con telerik

---
+++ excel2/RadForm1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace excel2
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radGridView1.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition2;
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(738, 400);
+            this.radGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 505);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radGridView1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ excel2/RadForm1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx;
+using Telerik.Windows.Documents.Spreadsheet.Formatting.FormatStrings;
+using Telerik.Windows.Documents.Spreadsheet.Model;
+
+namespace excel2
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XlsxFormatProvider formatProvider = new XlsxFormatProvider();
+            Workbook workbook = formatProvider.Import(File.ReadAllBytes(@"C:\datos\rascon\datos.xlsx"));
+
+            var worksheet = workbook.Sheets[0] as Worksheet;
+            var table = new DataTable();
+
+
+            //for (int i = 0; i < worksheet.UsedCellRange.ColumnCount; i++)
+            //{
+            //    CellSelection selection = worksheet.Cells[0, i];
+            //    var columnName = selection.GetValue().Value.RawValue.ToString();
+
+            //    table.Columns.Add(columnName);
+            //}
+            table.Columns.Add("I");
+            table.Columns.Add("CUIT");
+            table.Columns.Add("Agente");
+            table.Columns.Add("Mes");
+            table.Columns.Add("Año");
+            table.Columns.Add("DDJJ");
+            table.Columns.Add("Rect");
+            table.Columns.Add("Comprobante");
+            table.Columns.Add("Tipo Comprobante");
+            table.Columns.Add("Régimen");
+            table.Columns.Add("Monto Sujeto");
+            table.Columns.Add("Alícuota");
+            table.Columns.Add("Monto Retenido");
+            table.Columns.Add("Fecha Retención");
+            table.Columns.Add("Tipo Operación");
+            table.Columns.Add("Fecha Constancia");
+            table.Columns.Add("DDNro ConstanciaJJ");
+            table.Columns.Add("Nro Constancia Original");
+            table.Columns.Add("F");
+
+
+            for (int i = 3; i < worksheet.UsedCellRange.RowCount; i++)
+            {
+                var values = new object[worksheet.UsedCellRange.ColumnCount];
+
+                for (int j = 0; j < worksheet.UsedCellRange.ColumnCount; j++)
+                {
+                    CellSelection selection = worksheet.Cells[i, j];
+
+                    ICellValue value = selection.GetValue().Value;
+                    CellValueFormat format = selection.GetFormat().Value;
+                    CellValueFormatResult formatResult = format.GetFormatResult(value);
+                    string result = formatResult.InfosText;
+
+                    values[j] = result;
+                }
+                table.Rows.Add(values);
+
+            }
+            //recorro otro excel
+            formatProvider = new XlsxFormatProvider();
+            workbook = formatProvider.Import(File.ReadAllBytes(@"C:\datos\rascon\datos2.xlsx"));
+            worksheet = workbook.Sheets[0] as Worksheet;
+
+            for (int i = 3; i < worksheet.UsedCellRange.RowCount; i++)
+            {
+                var values = new object[worksheet.UsedCellRange.ColumnCount];
+
+                for (int j = 0; j < worksheet.UsedCellRange.ColumnCount; j++)
+                {
+                    CellSelection selection = worksheet.Cells[i, j];
+
+                    ICellValue value = selection.GetValue().Value;
+                    CellValueFormat format = selection.GetFormat().Value;
+                    CellValueFormatResult formatResult = format.GetFormatResult(value);
+                    string result = formatResult.InfosText;
+
+                    values[j] = result;
+                }
+                table.Rows.Add(values);
+
+            }
+
+
+            radGridView1.DataSource = table;
+            // Step 1: Convert a DataTable to Workbook
+            DataTableFormatProvider provider = new DataTableFormatProvider();
+
+            Workbook workbook2 = new Workbook();
+            Worksheet worksheet2 = workbook2.Worksheets.Add();
+
+            provider.Import(table, worksheet2);
+
+            // Step 2: Save Workbook as Excel file
+            IWorkbookFormatProvider formatProvider2 = new XlsxFormatProvider();
+
+            using (Stream output = new FileStream("union.xlsx", FileMode.Create))
+            {
+                formatProvider2.Export(workbook2, output);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
